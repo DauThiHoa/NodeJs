bf0023194736dcb233e2d4eaae132d3a59770369
mydung <91530841+TaoThiMyDung@users.noreply.github.com>
2023-04-14 17:24:26 +0700

    test

--- src/services/userService.js
+++ src/services/userService.js
@@ -132,28 +132,31 @@ let createNewUser = (data) => {
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already is used, Please try another email!'
+                    errMessage: 'Your email is already is used, Please try another email!'
                 })
+            }else{
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phonenumber: data.phonenumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
+                })
+            
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phonenumber: data.phonenumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId,
-            })
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            })
         } catch (e) {
             reject(e);
         }
     })
+            
 }
 let deleteUser = (userId) => {
     return new Promise(async (resolve, reject) => {
@@ -221,5 +224,6 @@ module.exports = {
     checkUserEmail: checkUserEmail,
     getAllUsers: getAllUsers,
     createNewUser: createNewUser,
-    deleteUser: deleteUser
+    deleteUser: deleteUser,
+    updateUserData: updateUserData
 }
